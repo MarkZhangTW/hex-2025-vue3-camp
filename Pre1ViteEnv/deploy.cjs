db2712e4949111a6f2b6f73178e4b10c378e2a99
Mark Zhang <MarkZhang1994@gmail.com>
2025-07-22 05:31:26 +0800

    Update deploy.cjs

--- Pre1ViteEnv/deploy.cjs
+++ Pre1ViteEnv/deploy.cjs
@@ -1,17 +1,20 @@
 const ghpages = require('gh-pages');
 const fs = require('fs-extra');
 const path = require('path');
-const dest = 'Pre1ViteEnv';
+
+const dest = path.basename(__dirname);
 const tmpdir = 'ghpages-tmp';
 
 ghpages.publish('dist', {
-    dest: tmpdir,
-    add: true,
-    async beforeAdd(git) {
-      fs.removeSync(path.join(git.cwd,'Pre1CDN'));
-      fs.removeSync(path.join(git.cwd,'Pre1ViteEnv'));
-      fs.moveSync(path.join(git.cwd, tmpdir), path.join(git.cwd, dest));
-    }
+  dest: tmpdir,
+  add: true,
+  async beforeAdd(git) {
+    fs.readdirSync(git.cwd)
+      .filter((file) => file !== tmpdir && file !== '.git')
+      .filter((file) => fs.statSync(path.join(git.cwd, file)).isDirectory())
+      .forEach((file) => fs.removeSync(path.join(git.cwd, file)));
+    fs.moveSync(path.join(git.cwd, tmpdir), path.join(git.cwd, dest));
+  }
 }, (err) => {
   if (err) {
     console.error('Deploy failed:', err);
