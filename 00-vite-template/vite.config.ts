e4e0174e13f1031ec9db10ecd565badf5eb1bbd4
Mark Zhang <MarkZhang1994@gmail.com>
2025-07-27 02:01:31 +0800

    build(vite): remove hash from output asset and entry filenames

--- 00-vite-template/vite.config.ts
+++ 00-vite-template/vite.config.ts
@@ -12,7 +12,15 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name][extname]',
+        entryFileNames: 'assets/[name].js',
+      },
     },
   },
 })
