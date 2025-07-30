946f1033480e24a792d67d02c2f106411d627ff3
Mark Zhang <markzhang1994@gmail.com>
2025-07-31 01:26:07 +0800

    feat: set base path for Vite configuration in vite.config.ts

--- 00-vue-2024-week1/vite.config.ts
+++ 00-vue-2024-week1/vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/hex-2025-vue3-camp/00-vue-2024-week1/',
   plugins: [
     vue(),
     vueDevTools(),
